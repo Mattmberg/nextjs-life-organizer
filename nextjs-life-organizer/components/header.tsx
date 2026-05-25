ab283d15051206816935640dd2a32c05f28968f6
Matthew Bergeron Jr <Matthewmckinley16@gmail.com>
2026-05-25 11:33:37 -0400

    Updated to create an initial build of the Header component.

--- nextjs-life-organizer/components/header.tsx
+++ nextjs-life-organizer/components/header.tsx
@@ -1,7 +1,11 @@
+type HeaderProps = {
+    
+};
+
 export default function Header() {
     return (
         <header>
-
+            <h1>Welcome to your Life Organizer!</h1>
         </header>
     );
 }
