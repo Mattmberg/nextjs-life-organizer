481816179e4a5877d076bd5031488f549b673efb
Matthew Bergeron Jr <Matthewmckinley16@gmail.com>
2026-05-25 21:29:06 -0400

    Updated footer component to use FooterProp for authors name.

--- nextjs-life-organizer/components/footer.tsx
+++ nextjs-life-organizer/components/footer.tsx
@@ -1,11 +1,11 @@
 type FooterProps = {
-    
+    authorName: string;
 };
 
-export default function Footer() {
+export default function Footer({authorName}: FooterProps) {
     return (
         <footer>
-            &copy; 2026 Author Name Here
+            &copy; 2026 {authorName}
         </footer>
     );
 }
