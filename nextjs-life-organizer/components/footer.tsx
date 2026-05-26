7eee6700ad40525c107bc79e43d93181ba880710
Matthew Bergeron Jr <Matthewmckinley16@gmail.com>
2026-05-25 20:46:31 -0400

    Built out initial Footer component.

--- nextjs-life-organizer/components/footer.tsx
+++ nextjs-life-organizer/components/footer.tsx
@@ -1,7 +1,11 @@
+type FooterProps = {
+    
+};
+
 export default function Footer() {
     return (
         <footer>
-            
+            &copy; 2026 Author Name Here
         </footer>
     );
 }
