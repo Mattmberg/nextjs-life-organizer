d1f9c4f996a5856254eed8e90e24d0cf5f7ba028
Matthew Bergeron Jr <Matthewmckinley16@gmail.com>
2026-05-26 07:58:57 -0400

    Updated Header component to use pageTitle prop.

--- nextjs-life-organizer/components/header.tsx
+++ nextjs-life-organizer/components/header.tsx
@@ -1,8 +1,8 @@
 type HeaderProps = {
-    
+    pageTitle: string;
 };
 
-export default function Header() {
+export default function Header({pageTitle}: HeaderProps) {
     return (
         <header>
             <h1>Welcome to your Life Organizer!</h1>
