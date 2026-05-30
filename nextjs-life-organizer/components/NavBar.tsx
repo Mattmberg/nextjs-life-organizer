f74beeadae6539dc1959e465db1ba5395313c3c7
Matthew Bergeron Jr <Matthewmckinley16@gmail.com>
2026-05-29 22:47:28 -0400

    Updated NavBar component.

--- nextjs-life-organizer/components/NavBar.tsx
+++ nextjs-life-organizer/components/NavBar.tsx
@@ -1,12 +1,36 @@
-type NavBarProps = {
-    navTo: string;
-    navName: string;
+export interface NavItem {
+    label: string;
+    href: string;
+    isExternal?: boolean;
+}
+
+interface NavBarProps {
+    websiteName: string;
+    links: NavItem[];
 };
 
-export default function NavBar(navTo: NavBarProps) {
+export default function NavBar({websiteName, links}: NavBarProps) {
     return (
-        <ul>
-            
-        </ul>
+        <nav>
+          <div className="">
+            <a href="/" className="">
+              {websiteName}
+            </a>
+          </div>
+
+          <div className="">
+            {links.map((link) => (
+              <a
+                key={link.href}
+                href={link.href}
+                target={link.isExternal ? '_blank' : '_self'}
+                rel={link.isExternal ? 'noopener noreferrer' : undefined}
+                className=""
+              >
+                {link.label}
+              </a>
+            ))}
+          </div>
+        </nav>
     );
 }
