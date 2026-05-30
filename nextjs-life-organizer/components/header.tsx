eaaa64443740eb25806719e457e719b983228009
Matthew Bergeron Jr <Matthewmckinley16@gmail.com>
2026-05-30 19:50:40 -0400

    Added NavBar component to the Header Component.

--- nextjs-life-organizer/components/header.tsx
+++ nextjs-life-organizer/components/header.tsx
@@ -1,3 +1,11 @@
+import NavBar from './NavBar';
+import { NavItem } from './NavBar';
+
+const navigationLinks: NavItem[] = [
+  { label: 'Home', href: '/' },
+  //{ label: 'About', href: '/about' },
+];
+
 type HeaderProps = {
     pageTitle: string;
 };
@@ -6,6 +14,8 @@ export default function Header({pageTitle}: HeaderProps) {
     return (
         <header>
             <h1>Welcome to your Life Organizer!</h1>
+
+            <NavBar websiteName="Life Organizer" links={navigationLinks} />
         </header>
     );
 }
