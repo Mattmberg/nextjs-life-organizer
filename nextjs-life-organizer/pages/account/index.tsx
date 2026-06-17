eef18c6e47803a1a8166be4d86ca50a397391a76
Matthew Bergeron Jr <84132501+Mattmberg@users.noreply.github.com>
2026-06-17 10:17:04 -0400

    Set up initial account page.

---
+++ nextjs-life-organizer/pages/account/index.tsx
@@ -0,0 +1,15 @@
+import Container from "@/components/container";
+import Footer from "@/components/footer";
+import Header from "@/components/header";
+
+export default function Account() {
+  return (
+    <>
+    <Container>
+        <Header pageTitle="Life Organizer"/>
+
+        <Footer authorName="Matthew Bergeron Jr" copyrightYear={2026}/>
+    </Container>
+    </>
+  );
+}
