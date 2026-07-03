4ceb86da9de8ba8fc74d5a9c7034d611e9327e1e
Matthew Bergeron Jr <84132501+Mattmberg@users.noreply.github.com>
2026-07-03 08:34:47 -0400

    Updated _app.tsx file to have the different necessary components.

--- nextjs-life-organizer/pages/_app.tsx
+++ nextjs-life-organizer/pages/_app.tsx
@@ -1,11 +1,13 @@
+import Footer from "@/components/footer";
 import "../assets/css/site.css";
 
 import type { AppProps } from "next/app";
 import Head from "next/head";
+import Header from "@/components/header";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
     return (
-        <>
+        <div className="contentContainer">
             <Head>
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
                 <meta
@@ -14,11 +16,12 @@ export default function MyApp({ Component, pageProps }: AppProps) {
                 />
                 <title>My Life Organizer</title>
             </Head>
+            
+            <Header siteTitle="Life Organizer"/>
 
+            <Component {...pageProps} />
 
-            <main className="">
-                <Component {...pageProps} />
-            </main>
-        </>
+            <Footer authorName="Matthew Bergeron Jr" copyrightYear={2026}/>
+        </div>
     );
 }
