58eff8ca726cfe26c8d960089ceb67608f9d3898
Matthew Bergeron Jr <84132501+Mattmberg@users.noreply.github.com>
2026-08-06 08:31:30 -0400

    Created an initial PreferenceList component for the preference page.

---
+++ nextjs-life-organizer/components/PreferenceList.tsx
@@ -0,0 +1,14 @@
+type PreferenceListProps = {
+    
+};
+
+export default function PreferenceList() {
+    return (
+        <>
+            <h3></h3>
+            <ol>
+                
+            </ol>        
+        </>
+    );
+}
