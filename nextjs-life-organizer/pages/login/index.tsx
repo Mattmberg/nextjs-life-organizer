b6ed78b7fbf5d4cd083c52e30998d93c6fc275cf
Matthew Bergeron Jr <84132501+Mattmberg@users.noreply.github.com>
2026-08-20 13:15:22 -0400

    Updated initial Login index page.

--- nextjs-life-organizer/pages/login/index.tsx
+++ nextjs-life-organizer/pages/login/index.tsx
@@ -1,14 +1,29 @@
-import Container from "@/components/container";
-import Footer from "@/components/footer";
-import Header from "@/components/header";
+import Container from "@/components/Container";
 
 export default function Login() {
   return (
     <>
     <Container>
-        <Header pageTitle="Life Organizer"/>
+      <main>
+        <h1>Log In</h1>
 
-        <Footer authorName="Matthew Bergeron Jr" copyrightYear={2026}/>
+        <h2>Welcome, please use your credentials to log in.</h2>
+
+        <form method="post">
+          <div id="form-group">
+            <label htmlFor="email">
+              Email Address
+            </label>
+            <input type="email" name="email" id="email"/>
+          </div>
+          <div id="form-group">
+            <label htmlFor="password">
+              Password
+            </label>
+            <input type="password" name="password" id="password"/>
+          </div>
+        </form>
+      </main>
     </Container>
     </>
   );
